c397ef54ed9e9f5a58f22d38ab720126b000980d
Navroop Singh <paperwarfare3@gmail.com>
2021-07-15 00:00:20 -0400

    answered question 1

---
+++ Question 1/question1.py
@@ -0,0 +1,29 @@
+from collections import OrderedDict
+from typing import List
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key):
+        if not key in self.cache:
+            return -1
+        value = self.cache.pop(key)
+        self.cache[key] = value
+        return value
+
+    def set(self, key, value):
+        if key in self.cache:
+            self.cache.pop(key)
+        elif len(self.cache) == self.capacity:
+            self.cache.popitem(last=False)
+        self.cache[key] = value
+
+def get_book_info(isbn):
+    pass
+
+def get_book_info_wrapper(isbn : str, cache: LRUCache) -> List[str, str, str]:
+    cache.set(isbn, get_book_info(isbn))
+    return cache.set(isbn)
+    
